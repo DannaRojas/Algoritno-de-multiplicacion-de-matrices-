657ebc565445dfdd8be07378cfc394984fd02d09
DannaRojas <65837321+DannaRojas@users.noreply.github.com>
2025-09-12 12:18:46 -0500

    Add files via upload

---
+++ modulo (1).h
@@ -0,0 +1,49 @@
+/**************************************************************************************
+ *                              Pontificia Universidad Javeriana
+ *
+ * Fecha: 11  de Septiembre 2025
+ * Autor: Danna Gabriela Rojas Bernal
+ * Materia: Sistemas Operativo
+ * Tema: Concurrencia
+ * Objetivo: Hacer una metodología para la implementación de la multiplicación de matrices.
+ * La idea principal, es construir paso a paso la implementación para hacer uso de la
+ * biblioteca PTHREAD. Se implementa el Algoritmo Clásico de multiplicación de matrices, para
+ * matrices cuadradas, es decir, la dimensión de filas es igual a la de columnas.
+ * A continuación se describen los paso
+ *  - Reserva de memoria
+ *      - Creación de punteros para matrices del tipo doble
+ *      - Asignación de memoria
+ *      - Ingreso de argumentos de entrada (Dimensión matriz, número de hilos)
+ *      - Validación argumentos de entrada
+ *      - Inicializar las matrices
+ *      - Imprimir las matrices
+ *      - Función para inicializar las matrices
+ *      - Función para imprimir las matrices
+ *      - Algoritmo clásico de multiplicación matrices
+ *  - Se verifica el resultado
+ *      - Función para multiplicación las matrices
+ *      - Declaración vector de hilos
+ *      - Creación de hilos según tamaño de vector de hilos
+ *      - Crear estructura de datos que encapsule los argumentos de entrada de la función MM
+ * Implementación de paralelismo: resolución de la multiplicación de matrices
+ *  - Se crea el vector de hilos
+ *  - Se tiene pendiente la exclusión de los hilos
+ *  - Se pasa a globales las matrices
+ *  - Encapsular los datos para enviarlos a la función MxM
+ *  - Se desencapsulan los datos dentro de la función MxM (descomprimen)
+*************************************************************************************/
+
+#ifndef __MODULO_H__
+#define __MODULO_H__
+
+double *mA, *mB, *mC;
+
+struct timeval ini, fin;
+
+void InicioMuestra();
+
+void FinMuestra();
+void iniMatriz(int n, double *m1, double *m2);
+void imprMatrices(int n, double *matriz);
+void multiMatrizClasica(double *m1, double *m2, int n);
+#endif
